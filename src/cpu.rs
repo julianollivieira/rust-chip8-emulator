85a6485545ff76d05da3e60aa8be72bfb4aa71c9
Julian Ollivieira <julian.ollivieira@gmail.com>
2021-10-19 23:12:34 +0200

    Finish drawing to the display (IBM logo ROM works now!)

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,4 +1,5 @@
-use crate::display::DISPLAY;
+use crate::display::{DISPLAY, HEIGHT, WIDTH};
+use std::borrow::BorrowMut;
 
 const FONT: [u8; 80] = [
     0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
@@ -20,14 +21,15 @@ const FONT: [u8; 80] = [
 ];
 
 pub struct CPU {
-    memory: [u8; 4096], // 4 kilobytes of RAM
-    pc: u16,            // program counter
-    i: u16,             // index register
-    stack: [u16; 16],   // stack
-    sp: u8,             // stack pointer
-    delay_timer: u8,    // delay timer (decrement 60 times a sec until 0)
-    sound_timer: u8,    // sound timer (same as delay_timer but beep while not 0)
-    v: [u8; 16],        // general purpose variable registers V0 through VF
+    memory: [u8; 4096],                 // 4 kilobytes of RAM
+    pc: u16,                            // program counter
+    i: u16,                             // index register
+    stack: [u16; 16],                   // stack
+    sp: u8,                             // stack pointer
+    delay_timer: u8,                    // delay timer (decrement 60 times a sec until 0)
+    sound_timer: u8,                    // sound timer (same as delay_timer but beep while not 0)
+    v: [u8; 16],                        // general purpose variable registers V0 through VF
+    pixels: [[bool; WIDTH]; HEIGHT]     // 64x32 display
 }
 
 impl CPU {
@@ -47,6 +49,7 @@ impl CPU {
             delay_timer: 0,
             sound_timer: 0,
             v: [0; 16],
+            pixels: [[false; WIDTH]; HEIGHT],
         }
     }
 
@@ -57,18 +60,17 @@ impl CPU {
         }
     }
 
-    pub fn step(&mut self, DISPLAY: &mut DISPLAY) {
+    pub fn step(&mut self, display: &mut DISPLAY) {
+
         // Fetch the instruction that PC is currently pointing at from memory
         let opcode: u16 = (self.memory[self.pc as usize] as u16) << 8
             | (self.memory[self.pc as usize + 1] as u16);
 
         let nnn: u16 = (opcode & 0x0FFF) as u16;            // 0000NNNN NNNNNNNN | lowest 12 bits
-        let kk: u8 = (opcode & 0x00FF) as u8;               // 00000000 KKKKKKKK | lowest 8 bits
+        let nn: u8 = (opcode & 0x00FF) as u8;               // 00000000 NNNNNNNN | lowest 8 bits
+        let n = (opcode & 0x000F) as usize;                 // 00000000 0000NNNN | lowest 4 bits
         let x = ((opcode & 0x0F00) >> 8) as usize;          // 0000XXXX 00000000 | lower 4 bits of the high byte
         let y = ((opcode & 0x00F0) >> 4) as usize;          // 00000000 YYYY0000 | upper 4 bits of the lower byte
-        let n = (opcode & 0x000F) as usize;                 // 00000000 0000NNNN | lowest 4 bits
-
-         // println!("opcode: {:X} | PC: {:#?}", opcode, self.pc);
 
         self.pc += 2;
 
@@ -76,33 +78,66 @@ impl CPU {
         match opcode & 0xF000 {
             0x0000 => match opcode & 0x00FF {
                 0x00E0 => {
+                    // [00E0] CLEAR
                     println!("CLEAR");
+                    self.pixels = [[false; WIDTH]; HEIGHT];
                 }
 
                 _ => panic!("Unimplemented opcode {:?}", opcode),
             },
 
             0x1000 => {
-                println!("JUMP");
+                // [1NNN] JUMP TO [NNN]
+                println!("JUMP TO [{:#0X}]", nnn);
+                self.pc = nnn;
             }
 
             0x6000 => {
-                println!("SET REGISTER VX");
+                // SET REGISTER [VX] TO [NN]
+                println!("SET REGISTER [V{:#}] TO [{:#0X}]", x, nn);
+                self.v[x] = nn;
             }
 
             0x7000 => {
-                println!("ADD VALUE TO REGISTER VX");
+                 // [7XNN] ADD [NN] TO REGISTER [VX]
+                println!("ADD [{:#0X}] TO REGISTER [V{:#}]", nn, x);
+                self.v[x] += nn;
             }
 
             0xA000 => {
-                println!("SET INDEX REGISTER I");
+                 // [ANNN] SET INDEX REGISTER I TO [NNN]
+                println!("SET INDEX REGISTER I TO [{:#0X}]", nnn);
+                self.i = nnn;
             }
 
             0xD000 => {
-                println!("DISPLAY / DRAW");
+                // [DXYN] DRAW SPRITE AT COORDINATE [VX, VY] WITH A HEIGHT OF [N]
+                println!("DRAW SPRITE AT [{}, {}] WITH HEIGHT OF [{}]", x, y, n);
+                self.draw(x, y, n, (*display).borrow_mut());
             }
 
             _ => panic!("Unimplemented opcode {:?}", opcode),
         }
     }
+
+    pub fn draw(&mut self, x: usize, y: usize, n: usize, display: &mut DISPLAY) {
+
+        let x_coord = self.v[x] as usize;
+        let y_coord = self.v[y] as usize;
+        let mut j = 0;
+        
+        for row in 0..n {
+            let sprite = self.memory[usize::from(self.i + j)];
+
+            for col in 0..8 {
+                let old_pixel = self.pixels[y_coord + row][x_coord + col];
+                let new_pixel = (sprite & (1 << (7 - col))) != 0; // ??
+                self.pixels[y_coord + row][x_coord + col] = old_pixel ^ new_pixel;
+            }
+
+            j += 1;
+        }
+        display.draw(&self.pixels);
+    }
+
 }
--- src/display.rs
+++ src/display.rs
@@ -7,8 +7,8 @@ use sdl2::Sdl;
 pub const WIDTH: usize = 64;
 pub const HEIGHT: usize = 32;
 pub const SCALE: u32 = 10;
-pub const FOREGROUND_COLOR: Color = Color::RGB(0, 0, 0);
-pub const BACKGROUND_COLOR: Color = Color::RGB(255, 255, 255);
+pub const FOREGROUND_COLOR: Color = Color::RGB(255, 255, 255);
+pub const BACKGROUND_COLOR: Color = Color::RGB(0, 0, 0);
 
 pub struct DISPLAY {
     canvas: Canvas<Window>,
@@ -34,24 +34,20 @@ impl DISPLAY {
         }
     }
 
-    pub fn draw(&mut self, pixels: &[[bool; 64]; 32]) {
+    pub fn draw(&mut self, pixels: &[[bool; WIDTH]; HEIGHT]) {
+        self.canvas.set_draw_color(BACKGROUND_COLOR);
+        self.canvas.clear();
+
+        self.canvas.set_draw_color(FOREGROUND_COLOR);
         for y in 0..HEIGHT {
             for x in 0..WIDTH {
                 if pixels[y][x] {
-                    self.canvas.set_draw_color(FOREGROUND_COLOR);
-                } else {
-                    self.canvas.set_draw_color(BACKGROUND_COLOR);
+                    let x = ((x as u32) * SCALE) as i32;
+                    let y = ((y as u32) * SCALE) as i32;
+                    self.canvas.fill_rect(Rect::new(x, y, SCALE, SCALE)).expect("Failed to draw pixel");
                 }
-
-                let x = ((x as u32) * SCALE) as i32;
-                let y = ((y as u32) * SCALE) as i32;
-
-                self.canvas
-                    .fill_rect(Rect::new(x, y, SCALE, SCALE))
-                    .expect("Failed to draw pixel");
             }
         }
-
         self.canvas.present();
     }
 }
